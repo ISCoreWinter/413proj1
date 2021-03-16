60499cff67a266f4616d86a8cc178d2a5c93edce
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2021-03-16 09:25:23 -0600

    addeed bootstrapping and basic conrtoller functionalities

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace _413proj1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,14 @@ namespace _413proj1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //this endpoint is to use the default file path routes of ASP.net
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
