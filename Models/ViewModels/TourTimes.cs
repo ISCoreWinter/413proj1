a43c77633b2797f34bced0dfbd1d85e0441f9760
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2021-03-16 23:31:08 -0600

    error checking

--- Models/ViewModels/TourTimes.cs
+++ Models/ViewModels/TourTimes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,8 @@ namespace _413proj1.Models.ViewModels
 {
     public class TourTimes
     {
+        [Key]
+        public int TourId { get; set; }
         public DayOfWeek Day { get; set; }
         public string StartTime { get; set; }
         public bool Reserved { get; set; }
