e2ebf4750d49017e4e7f8656b8ca3c660e6693c3
Kennedy Daniel <kenn97@byu.edu>
2021-03-17 18:37:17 -0600

    commit

---
+++ Models/ViewModels/ViewAppts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using OnlineBookstore.Models.ViewModels;
+
+namespace _413proj1.Models.ViewModels
+{
+    public class ViewAppts
+    {
+        public TourTimes Tours { get; set; }
+        public IEnumerable<ReservationInformation> BookedReservations { get; set; }
+    }
+}
